e1f3fad1f7f95a672ac8aa0c97ebfdab959e7489
SirenitaV <sirenita.violetta@gmail.com>
2021-09-02 20:32:06 +0500

    feat: tests positive

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,28 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.min = this.mid = this.max = 0;      
+    }
 
-    setRange(min, max) {
-
+    setRange(min, max) { 
+        this.max = max;
+        this.min = min;
     }
 
     guess() {
-
+        return this.mid = Math.round(this.min + (this.max - this.min) / 2);
+         
     }
 
     lower() {
-
+        this.max = this.mid;
+        return this.mid = Math.round(this.min + (this.max - this.min) / 2);
     }
 
     greater() {
-
+        this.min = this.mid;
+        this.mid = Math.round(this.min + (this.max - this.min) / 2);
     }
+        
 }
 
 module.exports = GuessingGame;
